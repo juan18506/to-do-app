ffb80f867c0046a7b13febd7cddbb6e9457b05e2
juan18506 <juan185067@gmail.com>
2023-08-23 18:25:57 -0300

    created & styled header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello world');
+var Months = /*#__PURE__*/function (Months) {
+  Months[Months["January"] = 0] = "January";
+  Months[Months["February"] = 1] = "February";
+  Months[Months["March"] = 2] = "March";
+  Months[Months["April"] = 3] = "April";
+  Months[Months["May"] = 4] = "May";
+  Months[Months["June"] = 5] = "June";
+  Months[Months["July"] = 6] = "July";
+  Months[Months["August"] = 7] = "August";
+  Months[Months["September"] = 8] = "September";
+  Months[Months["October"] = 9] = "October";
+  Months[Months["November"] = 10] = "November";
+  Months[Months["December"] = 11] = "December";
+  return Months;
+}(Months || {});
+var date = document.getElementById('date');
+var todoStatus = document.getElementById('status');
+var actualDate = new Date(Date.now());
+var month = Months[actualDate.getMonth()];
+var day = actualDate.getDate();
+var year = actualDate.getFullYear();
+date.innerHTML = "".concat(month, " ").concat(day, ", ").concat(year);
+todoStatus.innerHTML = "".concat(1, " incomplete, ", 5, " completed");
--- ts/app.ts
+++ ts/app.ts
@@ -1,3 +1,27 @@
 'use strict'
 
-console.log('hello world')
+enum Months {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+const date = document.getElementById('date') as HTMLParagraphElement
+const todoStatus = document.getElementById('status') as HTMLParagraphElement
+
+const actualDate = new Date(Date.now())
+const month = Months[actualDate.getMonth()]
+const day = actualDate.getDate()
+const year = actualDate.getFullYear()
+
+date.innerHTML = `${month} ${day}, ${year}`
+todoStatus.innerHTML = `${1} incomplete, ${5} completed`
